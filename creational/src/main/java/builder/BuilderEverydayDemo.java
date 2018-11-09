7473a59e92cb6070778cd2700e0b4b31e0fcca86
Vladimir Bredikhin <vladimir.bredikhin@broscorp.net>
2018-11-09 12:22:05 +0200

    creational: everyday builder demo of StringBuilder

---
+++ creational/src/main/java/builder/BuilderEverydayDemo.java
@@ -0,0 +1,16 @@
+package builder;
+
+public class BuilderEverydayDemo {
+
+  public static void main(String[] args) {
+
+    StringBuilder builder = new StringBuilder();
+    builder.append("This is an example ");
+    builder.append("of the builder patter. ");
+    builder.append("It has methods to append ");
+    builder.append("almost anything we want to a String. ");
+    builder.append(42);
+
+    System.out.println(builder.toString());
+  }
+}
