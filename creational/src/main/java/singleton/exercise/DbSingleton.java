a9bef3e6d382d3da5f10741f256bb73a57f5ac01
Vladimir Bredikhin <vladimir.bredikhin@broscorp.net>
2018-11-09 11:17:30 +0200

    creational: made singleton lazily loaded

--- creational/src/main/java/singleton/exercise/DbSingleton.java
+++ creational/src/main/java/singleton/exercise/DbSingleton.java
@@ -2,12 +2,15 @@ package singleton.exercise;
 
 class DbSingleton {
 
-  private static DbSingleton instance = new DbSingleton();
+  private static DbSingleton instance;
 
   private DbSingleton() {
   }
 
   static DbSingleton getInstance() {
+    if (instance == null) {
+      instance = new DbSingleton();
+    }
     return instance;
   }
 
