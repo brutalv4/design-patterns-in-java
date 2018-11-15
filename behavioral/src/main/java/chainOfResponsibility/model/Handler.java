2df30fbe867d7dc4b4d7fcb5c068c5d770bd31dd
Vladimir Bredikhin <vladimir.bredikhin@broscorp.net>
2018-11-15 15:51:32 +0200

    behavioral: ChainOfResponsibilityDemo

---
+++ behavioral/src/main/java/chainOfResponsibility/model/Handler.java
@@ -0,0 +1,12 @@
+package chainOfResponsibility.model;
+
+public abstract class Handler {
+
+  Handler successor;
+
+  public void setSuccessor(Handler successor) {
+    this.successor = successor;
+  }
+
+  public abstract void handleRequest(Request request);
+}
---
+++ behavioral/src/main/java/chainOfResponsibility/model/RequestType.java
@@ -0,0 +1,5 @@
+package chainOfResponsibility.model;
+
+public enum RequestType {
+  CONFERENCE, PURCHASE
+}
